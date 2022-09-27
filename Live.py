73a26dafca73c6e60e60f7ac7d87fd15f79b0b3f
shayrm <shayros@gmail.com>
2022-09-27 13:15:48 +0300

    Update MainGame file

--- Live.py
+++ Live.py
@@ -1,29 +1,29 @@
 
-
 def welcome(name):
     print (f"Hello {name} and welcome to the World of Games (WoG).\n Here you can find many cool games to play.")
 
 def generic_error():
-    print("Not a good selection, please enter a number! Try again.")
+    print("Not a good selection, please enter a valid option! Try again.")
     return
 
-def game_level():
+def input_validation(message, max_value):
     while True:
         try:
-            game_level = int(input("Please choose game difficulty from 1 to 5:"))
-            if (game_level <= 0) or (game_level >= 6):
+            user_input = int(input(f"{message}"))
+            if (user_input <= 0) or (user_input >= max_value):
                 generic_error()
                 continue
             else:
-                print(f"got the option {game_level}")
+                print(f"User choose option number {user_input}")
         except ValueError:
             generic_error()
             continue
         else:
-            return game_level
+            return user_input
             break
+
 def load_game():
-    message = """
+    message_options = """
     Please choose a game to play: 
     1. Memory Game 
         - a sequence of numbers will appear for 1 second and you have to guess it back 
@@ -33,23 +33,18 @@ def load_game():
     
     3. Currency Roulette 
         - try and guess the value of a random amount of USD in ILS 
-
+    
+    Please enter your option:
+    
     """
 
-    print(message)
+    max_option = 4
+    game_option = input_validation(message_options, max_option)
 
-    while True:
-        try:
-            user_choice = int(input("Please enter your choice:"))
-            if (user_choice <=0) or (user_choice >=4):
-                generic_error()
-                continue
-            else:
-                print(f"got the option {user_choice}")
-        except ValueError:
-            generic_error()
-            continue
-        else:
-            return user_choice
-            break
+    message_level = """
+    Please choose game difficulty from 1 to 5:
+    """
+    max_difficulty = 6
+    game_level = input_validation(message_level, max_difficulty)
 
+    return game_option, game_level
--- MainGame.py
+++ MainGame.py
@@ -1,10 +1,10 @@
 # main file for WorldOfGames
 
-from Live import welcome, load_game, game_level
+from Live import welcome, load_game
 
 if __name__ == '__main__':
+
     welcome("Shay")
-    game_selected = load_game()
+    [game_selected, game_level] = load_game()
     print(f"OK, let's play game number: {game_selected}")
-    user_lavel = game_level()
-    print(f"OK, set the game level to: {user_lavel}")
+    print(f"The game level was set to: {game_level}")
