377f504c751942d5c922a791f738fc8415e01053
shayrm <shayros@gmail.com>
2022-12-24 18:38:10 +0200

    update 2e2.py

--- Utils.py
+++ Utils.py
@@ -9,6 +9,9 @@ before a new game starts).
 """
 
 import os
+import platform
+import sys
+
 
 # Globals
 SCORES_FILE_NAME="scores.txt"
@@ -17,6 +20,7 @@ BAD_RETURN_CODE=0
 HOST_IP="127.0.0.1"
 HOST_FLASK_PORT=5000
 HOME_URL=f"http://{HOST_IP}:{HOST_FLASK_PORT}/"
+HOME_PATH = os.path.abspath("./")
 
 # WEB PAGE ELEMENT
 ELEMENT_ID_SCORE="score"
@@ -24,3 +28,18 @@ def screen_cleaner():
   # Clearing the Screen
   os.system('cls')
 
+
+
+
+def find_os():
+  system = platform.system()
+  return system
+
+  #if system == "Windows":
+  #    # code to run on Windows
+  #elif system == "Linux":
+  #    # code to run on Linux
+  #elif system == "Darwin":
+  #    # code to run on macOS
+  #else:
+  #    # code to run on other platforms
--- e2e.py
+++ e2e.py
@@ -13,25 +13,52 @@ Functions
     
 """
 from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
+from time import sleep
 import Utils
 import os
 
+#### ERROR!
+### Could not run the test. got PATH error for Chromdriver 
+### Need to set an absolute path and fix the get_element by ID
+#### 
+
+
 def test_scores_service(app_url, element_id):
   # Open a web browser and navigate to a webpage
-  driver = webdriver.Chrome()
-  driver.get(app_url)
+  os_type = Utils.find_os()
+  
+  if os_type == "Windows":
+    #chrome_driver = os.path.join(Utils.HOME_PATH, "chromedriver.exe")
+    chrome_driver = "C:\MyStuff\Info\DevOps_Course\WorldOfGames\chromedriver.exe"
+    print(f"Current path is: {chrome_driver}")
+    
+  elif os_type == "Linux":
+    chrome_driver = os.path.join(Utils.HOME_PATH, "chromedriver")
+  else:
+    print(f"OS type equal to: {os_type} and it is not supported")
+    exit
+      
+  chrome_services = Service(chrome_driver)
+  #chrome_options = Options()  
+  #driver_chrome = webdriver.Chrome(service=chrome_services, options=chrome_options)
+  driver_chrome = webdriver.Chrome(service=chrome_services)
+  driver_chrome.get(app_url)
 
   # Find an element with the ID "element-id"
-  element = driver.find_element_by_id(element_id)
+  element = driver_chrome.find_element_by_id(element_id)
   element_value = element.text()
   print(f"Got element value for score = {element_value}")
-  
+  sleep(15)
 
 def main():
   app_url = Utils.HOME_URL
   element_id = Utils.ELEMENT_ID_SCORE
   test_result = test_scores_service(app_url, element_id)
+  print(f" Got the latest results: {test_result}")
 
 if __name__ == "__main__":
   main()  
